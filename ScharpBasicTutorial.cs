60eba3796d1e7b5e7571f6191b15b37fda286f8d
Katuramu-Nic <katuramu.ict@gmail.com>
2022-01-11 12:50:45 +0300

    CsharpBasicTutorial Added
    I was able to debug a few errors

---
+++ ScharpBasicTutorial.cs
@@ -0,0 +1,12 @@
+using Systems;
+namespace ChsharpbasicTutorial;
+{
+    class Program
+    {
+static void Main(string[]args)
+{
+    //This is a comment, it just text for informing developers what the code does 
+    Console.Writeline("Hello World");
+}
+    }
+}
